5be1244bc0d7e882dd439359ba1932d673f7d952
Calvin Metcalf <cmetcalf@appgeo.com>
2015-03-20 16:03:54 -0400

    changes

--- benchmark/body-10000.js
+++ benchmark/body-10000.js
@@ -10,14 +10,16 @@ var seedrandom = require('seedrandom')
  * Globals for benchmark.js
  */
 
-global.buffer = getbuffer(10000)
+global.buffer = getbuffer(10000);
 global.cipher = require('..')
-global.string = getbuffer(10000).toString()
 
 var suite = new benchmark.Suite
 
-var encoding = [ 'hex', 'binary', 'base64' ];
-var ciphers = require('crypto').getCiphers();
+var encoding = [true, false];
+var ciphers = ['aes-128-ctr'];
+ciphers.push('chacha20');
+ciphers.push('chacha');
+//ciphers.push('chacha20/poly1305')
 for (var i = 0, ii = ciphers.length; i < ii; i++) {
     var j = 2;
     // for (var j = 0, jj = encoding.length; j < jj; j++) {
@@ -27,18 +29,22 @@ for (var i = 0, ii = ciphers.length; i < ii; i++) {
     // fn: 'var val = cipher(string, "' + ciphers[i] + '", "' + encoding[j]
     // + '")'
     // });
+    // suite.add({
+    //     name: ' js-' + ciphers[i],
+    //     minSamples: 100,
+    //     fn: 'var val = cipher(buffer, "' + ciphers[i] +'",true)'
+    // });
     suite.add({
-        name: ' buffer-' + encoding[j] + '-' + ciphers[i],
-        minSamples: 50,
-        fn: 'var val = cipher(buffer, "' + ciphers[i] + '", Buffer("ciao"), "'
-                + encoding[j] + '")'
+        name: ciphers[i],
+        minSamples: 1000,
+        fn: 'var val = cipher(buffer, "' + ciphers[i] +'",false)'
     });
     // }
 }
 
 suite.on('start', function onCycle(event) {
 
-    process.stdout.write('  10000 body\n\n')
+    process.stdout.write('  10000 byte buffer\n\n')
 })
 
 suite.on('cycle', function onCycle(event) {
--- index.js
+++ index.js
@@ -13,14 +13,17 @@
  * initialize module
  */
 // import
-var crypto = require('crypto');
+var cryptonative = require('browserify-aes');
+var cryptojs = require('browserify-aes/browser');
+var chachaNative = require('chacha');
+var chachajs = require('chacha/browser');
 
 /*
  * functions
  */
 /**
  * main
- * 
+ *
  * @exports ciphers
  * @function ciphers
  * @param {String|Buffer} raw - Check README.md
@@ -29,9 +32,66 @@ var crypto = require('crypto');
  * @param {String} encoding - Check README.md
  * @return {Object}
  */
-function ciphers(raw, cipher, password, encoding) {
-
-  var out = crypto.createCipher(cipher, password);
-  return out.update(raw, 'utf8', encoding) + out.final(encoding);
+var emptyBuffer = new Buffer('');
+var key128 = new Buffer(16);
+var key192 = new Buffer(24);
+var key256 = new Buffer(32);
+var iv16 = new Buffer(16);
+var iv12 = new Buffer(12);
+var iv8 = new Buffer(8);
+key128.fill(0);
+key192.fill(0);
+key256.fill(0);
+iv16.fill(0);
+iv12.fill(0);
+iv8.fill(0);
+function ciphers(raw, cipher, js) {
+  var auth = (cipher.indexOf('gcm') > -1);
+  if (cipher === 'chacha20/poly1305' || cipher === 'chacha20') {
+    auth = true;
+  }
+  var key;
+  if (cipher.indexOf('128') > -1) {
+    key = key128;
+  } else if (cipher.indexOf('192') > -1) {
+    key = key192;
+  } else {
+    key = key256;
+  }
+  var iv;
+  if (cipher.indexOf('ecb') > -1) {
+    iv = emptyBuffer;
+  } else if (auth) {
+    iv = iv12;
+  } else if (cipher === 'chacha') {
+    iv = iv8;
+  } else {
+    iv = iv16;
+  }
+  var crypto;
+  if (js) {
+    if (cipher === 'chacha20') {
+      crypto = chachajs.chacha20;
+    } else if (cipher === 'chacha20/poly1305') {
+      crypto = chachajs.aead;
+    } else {
+      crypto = cryptojs.createCipheriv;
+    }
+  } else {
+    if (cipher === 'chacha20') {
+      crypto = chachaNative.chacha20;
+    } else if (cipher === 'chacha20/poly1305') {
+      crypto = chachaNative.aead;
+    } else {
+      crypto = cryptonative.createCipheriv;
+    }
+  }
+  var out;
+  if (cipher === 'chacha20' || cipher === 'chacha20/poly1305') {
+    out = new crypto(key, iv);
+  } else {
+    out = crypto(cipher, key, iv);
+  }
+  return out.update(raw);
 }
 module.exports = ciphers;
